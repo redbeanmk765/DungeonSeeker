abb3773d2f709ebe2f93399c832fac3b95a319a4
redbeanmk765 <127715673+redbeanmk765@users.noreply.github.com>
2023-07-05 05:38:15 +0900

    move

---
+++ DungeonSeeker/Assets/Player/Scripts/MoveController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public float MaxSpeed;
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float Hor = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector2.right * Hor, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > MaxSpeed)
+        {
+            rigid.velocity = new Vector2(MaxSpeed, rigid.velocity.y);
+        }
+
+        else if (rigid.velocity.x < -1 * MaxSpeed)
+        {
+            rigid.velocity = new Vector2(-1 *  MaxSpeed, rigid.velocity.y);
+        }
+    }
+}
