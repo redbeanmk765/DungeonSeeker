c2cdb5d40709c0bb991f36aa6f606469e672e985
redbeanmk765 <127715673+redbeanmk765@users.noreply.github.com>
2023-10-30 06:56:12 +0900

    book2

--- DungeonSeeker/Assets/Monster/magicBook/magicBookCircle.cs
+++ DungeonSeeker/Assets/Monster/magicBook/magicBookCircle.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class magicBookCircle : MonoBehaviour
 {
-    public Vector3[] pos;
-    public float posX;
-    public float posY;
-    public bool IsStuck;
-    public int layermask;
+    public List<Vector2> pos;
     public int enemyDamage;
-    public float angle;
-    public float distance;
     public float projectileSpeed;
     public GameObject player;
     public GameObject magic;
@@ -22,12 +16,18 @@ public class magicBookCircle : MonoBehaviour
     void Start()
     {
         this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
-    }
+        pos = new List<Vector2> {
+            new Vector2(0f,1f),  new Vector2(0.35f, 0.65f), new Vector2(0.65f, 0.35f), 
+            new Vector2(0f, -1f),  new Vector2(0.35f, -0.65f), new Vector2(0.65f, -0.35f), 
+            new Vector2(1f, 0f),  new Vector2(-0.35f, -0.65f), new Vector2(-0.65f, -0.35f), 
+            new Vector2(-1f, 0f),  new Vector2(-0.35f, 0.65f), new Vector2(-0.65f, 0.35f)
+        };
+}
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
 
@@ -38,17 +38,20 @@ public class magicBookCircle : MonoBehaviour
 
     public void shootFire()
     {
-        enemyProjectile = Instantiate(magic);
-        enemyProjectile.transform.position = this.transform.position;
-        enemyProjectile.gameObject.GetComponent<targetEnemyProjectile>().target = player;
-        enemyProjectile.gameObject.GetComponent<targetEnemyProjectile>().dmg = this.enemyDamage;
-        enemyProjectile.gameObject.GetComponent<targetEnemyProjectile>().speed = this.projectileSpeed;
+        for (int i = 0; i <= 11; i++)
+        {
+            enemyProjectile = Instantiate(magic);
+            enemyProjectile.transform.position = this.transform.position;
+            enemyProjectile.gameObject.GetComponent<enemyProjectile>().pos = pos[i];
+            enemyProjectile.gameObject.GetComponent<enemyProjectile>().dmg = enemyDamage;
+            enemyProjectile.gameObject.GetComponent<enemyProjectile>().speed = projectileSpeed;
+        }
 
     }
 
     public void shootFire2()
     {
-        Destroy(this.gameObject);
+        //Destroy(this.gameObject);
     }
 }
 
